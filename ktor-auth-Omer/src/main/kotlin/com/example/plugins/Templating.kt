4fec516f6bdb03cf601e7fe9610eab0b343e74f3
aynac <omer.aynaci@hva.nl>
2023-11-15 15:21:45 +0100

    project created

---
+++ ktor-auth-Omer/src/main/kotlin/com/example/plugins/Templating.kt
@@ -0,0 +1,10 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.html.*
+import kotlinx.html.*
+
+fun Application.configureTemplating() {
+    routing {
+    }
+}
